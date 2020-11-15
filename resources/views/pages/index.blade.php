002c54314ae3c44c8b61b41f7f06ce47f8de5f33
jannath <jannathsyeda7@gmail.com>
2020-11-16 04:59:13 +0600

    category and subCategory

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+
+
+<div class="Container-fluid" >
+    <div class="Container" >
+        <div class="row">
+            <div class="col-md-12" style="background-color: #ccc;">
+                jj
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+@endsection
